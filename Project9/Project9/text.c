a9b1c24497c37b327d691c6de76bcf819e9b8541
王彦逸 <2171498913@qq.com>
2025-12-13 23:18:34 +0800

    基础回顾

---
+++ Project9/Project9/text.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+////{//for—≠ª∑
+	//int i;
+	//for (i = 1; i <= 100; i++)
+	//	printf("ƒ„∫√\n");
+	//int a;
+	//int sum=0;
+	//for (a = 0; a <= 100; a++)
+	//
+	//	sum = sum + a;
+	//
+	//	printf("%d", sum);
+	//int a=1;
+	//int b=1;
+	//int c;
+	//for (a = 1; a <= 9; a++)
+	//{
+	//	for (b = 1; b <= a; b++)
+	//	{
+	//		c = a * b;
+	//		printf("%d*%d=%d ", b, a, c);
+	//		
+	//	}
+	//	printf("\n");
+	//}
+	//int a = 1;
+	//while (a<=10)
+	//{
+	//	printf("ƒ„∫√\n");
+	//	a++;
+	//}
+	//int a = 1;
+	//int sum = 0;
+	//while (a <= 100)`
+	//{
+	//	sum = sum + a;
+	//	a++;
+	//}
+	//printf("%d", sum);
+	//int a =1;
+	//int b;
+	//while (a <= 9)
+	//{
+	//	b = 1;
+	//	while (b <= a)
+	//	{
+	//	
+	//		printf("%d*%d=%d ", b,a, a * b);	
+	//		b++;
+	//	
+	//	}printf("\n");
+	//	a++;
+	//}
+
+	//
+//	return 0;
+//}
+int main()
+{
+	int a = 100;
+	do
+	{
+		a -= 1;
+	} while (a >=0);
+	printf("%d\n", a);
+	return 0;
+	
+}
